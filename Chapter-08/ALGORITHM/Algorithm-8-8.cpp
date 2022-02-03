08e540bc620ff274aa1cbc42128a0b183c43dca8
Sayyid Mohammad Firoz Mahmud <61631664+Feroz455@users.noreply.github.com>
2022-02-03 15:32:16 +0600

    Add files via upload

---
+++ Chapter-08/ALGORITHM/Algorithm-8-8.cpp
@@ -0,0 +1,160 @@
+/*
+page 291
+Procedure 8.8 : DELEDGE(NODE, NEXT, ADJ, START, DEST, LINK, AVAILE, A, B, FLAG)
+
+  This procedure deletes the edge (A, B) from the graph G.
+
+1. Call FIND(NODE, NEXT, START, A, LOCA). [Locates node A.]
+
+2. Call FIND(NODE, NEXT, START, B, LOCB). [Locates node B.]
+
+3. Call DELETE(DEST, LINK, ADJ[LOCA], AVAILE, LOCB, FLAG).
+
+[Uses Procedure 8.4.]
+
+4. Return.
+*/
+#include <iostream>
+#include <list>
+#include <algorithm>
+using namespace std;
+void PRINT(list<int> A, list<int> X[])
+{
+    list<int>::iterator ptr = A.begin();
+    cout << "\n==========GRAPH=============\n";
+    while (ptr != A.end())
+    {
+        list<int>::iterator ptr1 = X[*ptr].begin();
+        cout << *ptr << " -> ";
+        while (ptr1 != X[*ptr].end())
+        {
+            cout << *ptr1++ << " ";
+        }
+        cout << "\n";
+        ptr++;
+    }
+
+    cout << "=======================\n";
+}
+void FINDEDGE(list<int> A, list<int> X[], int SRC, int DEST, int NODE)
+{
+    if (SRC > NODE)
+    {
+        return;
+    }
+    else
+    {
+        list<int>::iterator ptr = find(A.begin(), A.end(), SRC);
+        if (ptr != A.end())
+        {
+            list<int>::iterator temp = find(X[*ptr].begin(), X[*ptr].end(), DEST);
+            if (temp != X[*ptr].end())
+            {
+                cout << "Match found\n";
+            }
+            else
+            {
+                cout << "NOT FOUND\n";
+            }
+        }
+        else
+        {
+            cout << "NOT FOUND\n";
+        }
+    }
+}
+void INSER_NODE(list<int> &A, list<int> X[], int &NODE)
+{
+    A.push_back(NODE);
+    NODE = NODE + 1;
+}
+void INSERT_EDGE(list<int> X[], int NODE, int &EDGE, int SRC, int DEST)
+{
+    if (SRC > NODE && SRC > DEST)
+    {
+        return;
+    }
+    else
+    {
+        X[SRC].push_back(DEST);
+        EDGE += 1;
+    }
+}
+void DELETE_EDGE(list<int> X[], int SRC, int DEST, int NODE, int &EDGE)
+{
+    if (SRC > NODE || DEST > NODE)
+        return;
+    else
+    {
+        list<int>::iterator ptr = find(X[SRC].begin(), X[SRC].end(), DEST);
+        if (ptr != X[SRC].end())
+        {
+            X[SRC].remove(DEST);
+            EDGE -= 1;
+        }
+        else
+            cout << "EDGE NOT EXIST\n";
+    }
+
+    return;
+}
+void DELETE_NODE(list<int> &A, list<int> X[], int &NODE, int node)
+{
+    list<int>::iterator ptr = A.begin();
+    while (ptr != A.end())
+    {
+        X[*ptr].remove(node);
+        ptr++;
+    }
+    X[node].clear();
+    A.remove(node);
+    NODE -= 1;
+}
+int main()
+{
+    list<int> A;
+    list<int> X[2000];
+    int EDGE, NODE, SRC, DEST;
+    cout << "Enter Nodes and EDGE\n";
+    cin >> NODE >> EDGE;
+
+    for (int i = 0; i < EDGE; i++)
+    {
+        cin >> SRC >> DEST;
+        X[SRC].push_back(DEST);
+    }
+    for (int i = 0; i < NODE; i++)
+    {
+        A.push_back(i);
+    }
+    PRINT(A, X);
+    DELETE_EDGE(X, 0, 2, NODE, EDGE);
+    PRINT(A, X);
+}
+/*
+Enter Nodes and EDGE
+5 7
+0 1
+0 2
+0 3
+1 2
+3 2
+3 4
+4 2
+
+==========GRAPH=============
+0 -> 1 2 3
+1 -> 2
+2 ->
+3 -> 2 4
+4 -> 2
+=======================
+
+==========GRAPH=============
+0 -> 1 3
+1 -> 2
+2 ->
+3 -> 2 4
+4 -> 2
+=======================
+*/
