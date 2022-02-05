9530c4abbeacdbeb962327156378aa35bc33e11f
Sayyid Mohammad Firoz Mahmud <61631664+Feroz455@users.noreply.github.com>
2022-02-05 22:38:45 +0600

    Add files via upload

---
+++ Chapter-08/SOLVED PROBLEMS/8-13.cpp
@@ -0,0 +1,68 @@
+/*
+page 308
+problem 8-13
+*/
+#include <iostream>
+#include <list>
+#include <algorithm>
+using namespace std;
+void PRINT(list<int> A, list<int> X[])
+{
+    list<int>::iterator ptr = A.begin();
+    cout << "\n==========GRAPH=============\n";
+    while (ptr != A.end())
+    {
+        list<int>::iterator ptr1 = X[*ptr].begin();
+        cout << char((*ptr)+65) << " -> ";
+        while (ptr1 != X[*ptr].end())
+        {
+            cout << char((*ptr1++)+65) << " ";
+        }
+        cout << "\n";
+        ptr++;
+    }
+
+    cout << "=======================\n";
+}
+int main()
+{
+    list<int> A;
+    list<int> X[2000];
+    int EDGE, NODE, SRC, DEST;
+    char n, m;
+    cout << "Enter Nodes and EDGE\n";
+    cin >> NODE >> EDGE;
+
+    for (int i = 0; i < EDGE; i++)
+    {
+        cin >> n >> m;
+        SRC = n - 65;
+        DEST = m - 65;
+        X[SRC].push_back(DEST);
+    }
+    for (int i = 0; i < NODE; i++)
+    {
+        A.push_back(i);
+    }
+    PRINT(A, X);
+}
+/*
+Enter Nodes and EDGE
+5 8
+A B
+A D
+B D
+B C
+B E
+C E
+D E
+E D
+
+==========GRAPH=============
+A -> B D
+B -> D C E
+C -> E
+D -> E
+E -> D
+=======================
+*/
