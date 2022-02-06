6020037473b9c9541bc0e63b3d7731168554ee71
Sayyid Mohammad Firoz Mahmud <61631664+Feroz455@users.noreply.github.com>
2022-02-06 11:56:32 +0600

    Add files via upload

---
+++ Chapter-08/SUPPLEMENTARY PROBLEMS/8-25.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+using namespace std;
+// DSU data structure
+//  path compression + rank by union
+ 
+class DSU
+{
+    int *parent;
+    int *rank;
+ 
+public:
+    DSU(int n)
+    {
+        parent = new int[n];
+        rank = new int[n];
+ 
+        for (int i = 0; i < n; i++)
+        {
+            parent[i] = -1;
+            rank[i] = 1;
+        }
+    }
+ 
+    // Find function
+    int find(int i)
+    {
+        if (parent[i] == -1)
+            return i;
+ 
+        return parent[i] = find(parent[i]);
+    }
+    // union function
+    void unite(int x, int y)
+    {
+        int s1 = find(x);
+        int s2 = find(y);
+ 
+        if (s1 != s2)
+        {
+            if (rank[s1] < rank[s2])
+            {
+                parent[s1] = s2;
+                rank[s2] += rank[s1];
+            }
+            else
+            {
+                parent[s2] = s1;
+                rank[s1] += rank[s2];
+            }
+        }
+    }
+};
+ 
+class Graph
+{
+    vector<vector<int>> edgelist;
+    int V;
+ 
+public:
+    Graph(int V)
+    {
+        this->V = V;
+    }
+ 
+    void addEdge(int x, int y, int w)
+    {
+        edgelist.push_back({w, x, y});
+    }
+ 
+    int kruskals_mst()
+    {
+        // 1. Sort all edges
+        sort(edgelist.begin(), edgelist.end());
+ 
+        // Initialize the DSU
+        DSU s(V);
+        int ans = 0;
+        for (auto edge : edgelist)
+        {
+            int w = edge[0];
+            int x = edge[1];
+            int y = edge[2];
+ 
+            // take that edge in MST if it does form a cycle
+            if (s.find(x) != s.find(y))
+            {
+                s.unite(x, y);
+                ans += w;
+            }
+        }
+        return ans;
+    }
+};
+int main()
+{
+    Graph g(4);
+    g.addEdge(0, 1, 1);
+    g.addEdge(1, 3, 3);
+    g.addEdge(3, 2, 4);
+    g.addEdge(2, 0, 2);
+    g.addEdge(0, 3, 2);
+    g.addEdge(1, 2, 2);
+ 
+    // int n, m;
+    // cin >> n >> m;
+ 
+    // Graph g(n);
+    // for (int i = 0; i < m; i++)
+    // {
+    //     int x, y, w;
+    //     cin >> x >> y >> w;
+    //     g.addEdge(x, y, w);
+    // }
+ 
+    cout << g.kruskals_mst();
+    return 0;
+}
