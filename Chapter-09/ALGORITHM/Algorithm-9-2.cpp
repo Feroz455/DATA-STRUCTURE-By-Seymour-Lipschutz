c80934484924320506d2cd3f64ed744324b0656f
Sayyid Mohammad Firoz Mahmud <61631664+Feroz455@users.noreply.github.com>
2022-02-07 14:37:11 +0600

    Update Algorithm-9-2.cpp

--- Chapter-09/ALGORITHM/Algorithm-9-2.cpp
+++ Chapter-09/ALGORITHM/Algorithm-9-2.cpp
@@ -1,34 +1,56 @@
+/*
+page 325
+Procedure 9.2: MIN(A, K, N, LOC)
+
+An array A is in memory. This procedure finds the location LOC of the 
+smallest element among A[K], A[K + 1], …, A[N].
+
+1.    Set MIN := A[K] and LOC := K. [Initializes pointers.]
+
+2.    Repeat for J = K + 1, K + 2, …, N:
+
+If MIN > A[J], then: Set MIN := A[J] and LOC := A[J] and LOC := J.
+
+[End of loop.]
+
+3.    Return.
+*/
 #include <iostream>
 using namespace std;
-template <typename T>
-void selection(T A[], int N)
+void MIN(int A[], int K, int N, int &LOC)
 {
-    int min;
-    T temp;
-    for (int i = 0; i < N-1; i++)
+    int min = A[K];
+    LOC = K;
+    for (int i = K + 1; i < N; i++)
     {
-        min = i;
-        for (int j = i + 1; j < N; j++)
+        if (min > A[i])
         {
-            if (A[j] < A[min])
-            {
-                min = j;
-            }
+            min = A[i];
+            LOC = i;
         }
+    }
+}
+void selection(int A[], int N)
+{
+    int LOC = 0;
+    int temp;
+    for (int i = 0; i < N - 1; i++)
+    {
+        MIN(A, i, N, LOC);
         temp = A[i];
-        A[i] = A[min];
-        A[min] = temp;
+        A[i] = A[LOC];
+        A[LOC] = temp;
     }
 }
 int main()
 {
-    int A[] = {2,7,4,1,5,3};
-    selection<int>(A, 6);
-    for(int i:A)
+    int A[] = {2, 7, 4, 1, 5, 3};
+    selection(A, 6);
+    for (int i : A)
     {
         cout << i << " ";
     }
 }
 /*
-1 2 3 4 5 7 
+1 2 3 4 5 7
 */
