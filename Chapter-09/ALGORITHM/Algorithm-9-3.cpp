99a5beb7a50de475c7ab07385c072a89e1c631a8
Sayyid Mohammad Firoz Mahmud <61631664+Feroz455@users.noreply.github.com>
2022-02-07 14:36:42 +0600

    Update Algorithm-9-3.cpp

--- Chapter-09/ALGORITHM/Algorithm-9-3.cpp
+++ Chapter-09/ALGORITHM/Algorithm-9-3.cpp
@@ -1,34 +1,58 @@
+/*
+page 325
+Algorithm 9.3: (Selection Sort) SELECTION(A, N)
+
+This algorithm sorts the array A with N elements.
+
+1.    Repeat Steps 2 and 3 for K = 1, 2, …, N – 1:
+
+2.           Call MIN(A, K, N, LOC).
+
+3.           [Interchange A[K] and A[LOC].]
+
+Set TEMP := A[K], A[K] := A[LOC] and A[LOC] := TEMP.
+
+[End of Step 1 loop.]
+
+4.    Exit.
+*/
+
 #include <iostream>
 using namespace std;
-template <typename T>
-void selection(T A[], int N)
+void MIN(int A[], int K, int N, int &LOC)
 {
-    int min;
-    T temp;
-    for (int i = 0; i < N-1; i++)
+    int min = A[K];
+    LOC = K;
+    for (int i = K + 1; i < N; i++)
     {
-        min = i;
-        for (int j = i + 1; j < N; j++)
+        if (min > A[i])
         {
-            if (A[j] < A[min])
-            {
-                min = j;
-            }
+            min = A[i];
+            LOC = i;
         }
+    }
+}
+void selection(int A[], int N)
+{
+    int LOC = 0;
+    int temp;
+    for (int i = 0; i < N - 1; i++)
+    {
+        MIN(A, i, N, LOC);
         temp = A[i];
-        A[i] = A[min];
-        A[min] = temp;
+        A[i] = A[LOC];
+        A[LOC] = temp;
     }
 }
 int main()
 {
-    int A[] = {2,7,4,1,5,3};
-    selection<int>(A, 6);
-    for(int i:A)
+    int A[] = {2, 7, 4, 1, 5, 3};
+    selection(A, 6);
+    for (int i : A)
     {
         cout << i << " ";
     }
 }
 /*
-1 2 3 4 5 7 
-*/ 
+1 2 3 4 5 7
+*/
