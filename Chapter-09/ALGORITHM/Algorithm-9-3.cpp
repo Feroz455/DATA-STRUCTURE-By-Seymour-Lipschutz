a04308fca4bd915da34bad32efedde764e480193
Sayyid Mohammad Firoz Mahmud <61631664+Feroz455@users.noreply.github.com>
2022-02-07 14:30:56 +0600

    Add files via upload

---
+++ Chapter-09/ALGORITHM/Algorithm-9-3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+template <typename T>
+void selection(T A[], int N)
+{
+    int min;
+    T temp;
+    for (int i = 0; i < N-1; i++)
+    {
+        min = i;
+        for (int j = i + 1; j < N; j++)
+        {
+            if (A[j] < A[min])
+            {
+                min = j;
+            }
+        }
+        temp = A[i];
+        A[i] = A[min];
+        A[min] = temp;
+    }
+}
+int main()
+{
+    int A[] = {2,7,4,1,5,3};
+    selection<int>(A, 6);
+    for(int i:A)
+    {
+        cout << i << " ";
+    }
+}
+/*
+1 2 3 4 5 7 
+*/ 
